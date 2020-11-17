7855a23ac35d093c3a9db222c41fe5f4db2a705b
PierreKLN <61541007+PierreKLN@users.noreply.github.com>
2020-11-17 17:29:50 +0100

    Create Patients.java

---
+++ ProjetJava/src/projetjava/Patients.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava;
+
+/**
+ *
+ * @author Pierr
+ */
+public class Patients {
+    
+    private String m_name;
+    private String m_surname;
+    private String m_adresse;
+    
+    public Patients()
+    {
+        
+    }
+    
+}
