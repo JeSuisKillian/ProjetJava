2a4832a592717b0833a9722dce12d45f053c2299
PierreKLN <61541007+PierreKLN@users.noreply.github.com>
2020-11-20 18:39:32 +0100

    ok

---
+++ ProjetJava/src/projetjava/Appointment.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Pierr
+ */
+public class Appointment {
+     
+    private GregorianCalendar date;
+    private Patients patient;
+    private Doctors doctor;
+    private String motif;
+    private boolean disponible;
+    
+    public Appointment (GregorianCalendar d, Doctors doc, String m, boolean dispo)
+    {
+        date = d;
+        doctor=doc;
+        motif = m;
+        disponible = dispo;
+    }
+}
+
